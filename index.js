76ff6a6b54c564da1315c7b10bc1d61e392f67f5
Sky <skyengineer001@gmail.com>
2025-02-05 16:43:45 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// Generate a random number between 1 and 100
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+// Run the function and log the result
+console.log("Random Number:", generateRandomNumber());
